22d29b53450eeeb992ab1c45e23b4fcb8eb3fc11
EDNAN FRIZZERA FILHO <ednan@frizzera.dev>
2024-03-19 08:35:57 -0300

    Fix unit button not being selected at start

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,14 @@
-function Button({ border, side, onClick, children }) {
+function Button({ border, side, onClick, isActive, children }) {
 	return (
 		<button
 			className={`${border} px-6 py-1 font-roboto 
+      
       hover:bg-blue-500 
       hover:text-slate-50 
       hover:rounded-${side}-lg 
-      
-      active:bg-blue-500 
-      active:text-slate-50
-      active:rounded-${side}-lg 
-      
-      focus-within:text-blue-500 
-      focus-within:rounded-${side}-lg 
-    
+
+      ${isActive}
+
       duration-200`}
 			onClick={onClick}
 		>
--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,18 +1,26 @@
 import Button from './Button';
 
-// TODO: setar as classes ativas como prop ao invés de deixar permanentemente, dessa forma da pra iniciar com um dos botões ativos
-
-function ButtonGroup({ changeSelectUnitsF, changeSelectUnitsC }) {
+function ButtonGroup({
+	selectedUnits,
+	changeSelectUnitsF,
+	changeSelectUnitsC,
+}) {
 	return (
 		<div className="bg-slate-50 rounded-lg shadow-md">
 			<Button
 				border="border-r-[1px] rounded-l-lg border-slate-300"
 				side={'l'}
 				onClick={changeSelectUnitsC}
+				isActive={selectedUnits === 'metric' ? 'text-blue-500' : ''}
 			>
 				ºC
 			</Button>
-			<Button border="rounded-r-lg" side={'r'} onClick={changeSelectUnitsF}>
+			<Button
+				border="rounded-r-lg"
+				side={'r'}
+				onClick={changeSelectUnitsF}
+				isActive={selectedUnits === 'imperial' ? 'text-blue-500' : ''}
+			>
 				ºF
 			</Button>
 		</div>
